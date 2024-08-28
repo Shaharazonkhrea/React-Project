bc22d5ef0e045e17260d643466a513aedb1aa4a0
Shaharazon Thomas <shaharazonthomas@gmail.com>
2024-08-28 02:31:21 -0400

    first commit for main components

---
+++ src/Cake.tsx
@@ -0,0 +1,22 @@
+// src/components/Cake.tsx
+
+import { FC } from 'react';
+import { Layer } from './Layer';
+
+interface CakeProps {
+  layers: Layer[];
+}
+
+const Cake: FC<CakeProps> = ({ layers }) => {
+  return (
+    <div>
+      {layers.map((layer, index) => (
+        <div key={index}>
+          Layer {index + 1} - Height: {layer.height}, Width: {layer.width}, Color: {layer.color}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Cake;
